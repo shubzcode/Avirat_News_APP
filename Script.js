86fcb5af4fa27407b948edeccf5f17257e9f587c
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-09-23 20:32:19 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 const API_KEY = "a1be73e6af2743c5b66a4ece6d999f00"
 const url = "https://newsapi.org/v2/everything?q="
 
-window.addEventListener('load', () => fetchNews("India"));
+window.addEventListener("load", () => fetchNews("India"));
 
 async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);

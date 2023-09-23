5c8a26ca81fd369e946f5f6faac67820cbf5edc2
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-09-23 20:21:34 +0530

    UPLOAD

---
+++ Script.js
@@ -0,0 +1,12 @@
+const API_KEY = "a1be73e6af2743c5b66a4ece6d999f00"
+const url = "https://newsapi.org/v2/everything?q="
+
+window.addEventListener('load', () => fetchNews("India"));
+
+async function fetchNews(query) {
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();
+    bindData(data.articles);
+}
+
+

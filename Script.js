06ab9c7f2e0c9a41fa60cd1849ad2081fd6d7800
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-09-23 22:34:56 +0530

    api

--- Script.js
+++ Script.js
@@ -8,3 +8,37 @@ async function fetchNews(query) {
     const data = await res.json();
     bindData(data.articles);
 }
+
+function bindData(articles) {
+    const cardContainer = document.getElementById('card-container');
+    const newsCardTemplate = document.getElementById('template-news-card');
+
+    cardContainer.innerHTML = "";
+
+    articles.forEach((article) => {
+        if(!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        cardContainer.appendChild(cardClone);
+    });
+}
+
+function fillDataInCard(cardClone,article) {
+    const newsImg = cardClone.querySelector('#news-img');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newsSource = cardClone.querySelector('#news-source');
+    const newsDesc = cardClone.querySelector('#news-desc');
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = articleTitle;
+    newsDesc.innerHTML = article.description;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta",
+    });
+
+    newsSource.innerHTML = `${article.source.name} · ${date}`;
+    
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+    });
+}
